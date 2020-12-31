e7b6620f0ce6d6208a509850f315c3753a57d210
Kolyat <kks.pub@gmail.com>
2021-01-01 02:50:19 +0300

    New Year 2021

--- prepare_db.py
+++ prepare_db.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Kirill 'Kolyat' Kiselnikov
+# Copyright (c) 2021 Kirill 'Kolyat' Kiselnikov
 # This file is the part of testutils, released under modified MIT license
 
 import os
